2b918ed182c813584f528c4832b8db0a2c6b8d73
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-05-22 14:58:52 +0200

    add javadoc comments to explain what the method in the StringUtils class does

--- src/main/java/at/fhj/msd/StringUtils.java
+++ src/main/java/at/fhj/msd/StringUtils.java
@@ -1,7 +1,15 @@
 package at.fhj.msd;
 
+/**
+ * the StringUtils class has a method that checks if a word is written in a reserved it would still be the same and if so the method returns true
+ */
 public class StringUtils {
 
+    /**
+     * the method checks if the word is still correct even when its written in the other way around (reversed)
+     * @param input the word that needs to be checked
+     * @return true if the word is still correct when reversed
+     */
     public static boolean isPalindrome(String input) {
         String reversed = new StringBuilder(input).reverse().toString();
         return input.equals(reversed);
